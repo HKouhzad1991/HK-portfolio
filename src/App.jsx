3e50b9cecd12732cb1b26ce7af18919dc9d7c464
hosseinkouhzad2017@gmail.com <https://github.com/HKouhzad1991>
2023-07-24 01:21:37 +0330

    app.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header/Header";
 
 const App = () => {
   //don't forget to add font link in index.html
-  return <Header />;
+  return <div className={css.container}>header</div>;
 };
 
 export default App;
