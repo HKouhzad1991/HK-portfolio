2bbf7b0804b961f1c7f0de86e3f2407032ee97cb
hosseinkouhzad2017@gmail.com <https://github.com/HKouhzad1991>
2023-07-25 22:02:56 +0330

    fixing header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,27 +5,23 @@ import { BiPhoneCall } from "react-icons/bi";
 const Header = () => {
   return (
     <div className={`paddings ${css.wrapper}`}>
-      
-      <div className={"flexCenter innerWidth ${css.container}"}>
-        <div className={css.name}>
-          Hossein-Kouhzad
-        </div>
-
-        <ul className={"flexCenter ${css.menu}"}>
+      <div className={`innerWidth ${css.container} flexCenter`}>
+        <div className={css.name}>Hossein kouhzad</div>
+        <ul className={`flexCenter ${css.menu}`}>
           <li>
-            <a href="">SERVICES</a>
+            <a href="#experties">Services</a>
           </li>
           <li>
-            <a href="">EXPERIENCE</a>
+            <a href="#work">Experience</a>
           </li>
           <li>
-            <a href="">PORTFOLIO</a>
+            <a href="#portfolio">Portfolio</a>
           </li>
           <li>
-            <a href="">TESTIMONIALS</a>
+            <a href="#people">Testimonials</a>
           </li>
-          <li>
-            <p>+989113191788</p>
+          <li className={`flexCenter ${css.phone}`}>
+            <p>+001 (313) 345 678</p>
             <BiPhoneCall size={"40px"} />
           </li>
         </ul>
