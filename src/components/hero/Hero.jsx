fcaa4dea142429b434eb5088d112415ec5e61ce4
hosseinkouhzad2017@gmail.com <https://github.com/HKouhzad1991>
2023-07-26 18:39:26 +0330

    upper and lower div for hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import css from "./Hero.module.scss";
 const Hero = () => {
-  return <section className={`paddings ${css.wrapper}`}>Hero</section>;
+  return (
+    <section className={`paddings ${css.wrapper}`}>
+      <div className={"innerWidth ${css.container}"}>
+        {/* upper element */}
+        <div className={css.upperElements}>
+          <span>
+            Hey There,
+            <br /> I'm Hossein.
+          </span>
+          <span>
+            I design beautiful simple things,
+            <br /> And I love what i do
+          </span>
+        </div>
+
+        {/* lower elements */}
+
+        <div className={css.lowerElements}>
+                  <div className={css.experience}>
+                  
+                  </div>
+          <div className={css.certificate}></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
