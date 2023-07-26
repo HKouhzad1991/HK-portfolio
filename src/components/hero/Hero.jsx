50f1537f9e57ce24a12e0a320e9f750fff2f6890
hosseinkouhzad2017@gmail.com <https://github.com/HKouhzad1991>
2023-07-26 18:27:04 +0330

    padding to wrapper

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import css from "./Hero.module.scss";
 const Hero = () => {
-  return <section className={CSS.wrapper}>Hero</section>;
+  return <section className={`paddings ${css.wrapper}`}>Hero</section>;
 };
 
 export default Hero;
