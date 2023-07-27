bda31242cac53ea6db4a5fc5d8180f7ab0ddb8e5
hosseinkouhzad2017@gmail.com <https://github.com/HKouhzad1991>
2023-07-27 17:19:57 +0330

    work component creating

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import People from "./components/People/People";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Hero from "./components/hero/Hero";
 import Footer from "./components/Footer/Footer";
+import Work from "./components/Work/Work";
 import css from "./styles/app.module.scss";
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Portfolio />
       <People />
       <Footer />
+      <Work />
     </div>
   );
 };
