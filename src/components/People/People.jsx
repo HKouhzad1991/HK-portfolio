f4768a9df47bf5b75499c4114f0f5467d303f4d6
hosseinkouhzad2017@gmail.com <https://github.com/HKouhzad1991>
2023-07-27 23:10:14 +0330

    people fixed

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -2,27 +2,24 @@ import React from "react";
 import { comments, sliderSettings } from "../../utils/data";
 import css from "./People.module.scss";
 import Slider from "react-slick";
-import { motion } from "framer-motion";
-import {
-  footerVariants,
-  staggerChildren,
-  textVariant,
-  textVariant2,
-} from "../../utils/motion";
+import {motion} from 'framer-motion'
+import { footerVariants, staggerChildren, textVariant, textVariant2 } from "../../utils/motion";
 const People = () => {
   return (
     <motion.section
-      variants={staggerChildren}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      section
-      className={`paddings ${css.wrapper}`}>
+    variants={staggerChildren}
+    initial="hidden"
+    whileInView="show"
+    viewport={{ once: false, amount: 0.25 }}
+    section className={`paddings ${css.wrapper}`}>
+
       <a className="anchor" id="people"></a>
 
       <motion.div
-        variants={footerVariants}
-        className={`yPaddings innerWidth ${css.container}`}>
+      variants={footerVariants}
+      className={`yPaddings innerWidth ${css.container}`}>
+
+
         <div className={`flexCenter ${css.heading}`}>
           <span className="primaryText">People talk about us</span>
           <p style={{ marginTop: "2rem" }}>
@@ -30,6 +27,8 @@ const People = () => {
           </p>
           <p>The process of submitting an appilication was quite cosy</p>
         </div>
+
+
 
         <div className={`yPaddings ${css.comments}`}>
           {/* to use slider , we have to inlcude css in index.html head */}
@@ -49,7 +48,10 @@ const People = () => {
             })}
           </Slider>
         </div>
+
+
       </motion.div>
+
     </motion.section>
   );
 };
